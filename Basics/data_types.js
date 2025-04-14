8d9a9f1e6516f549b3154754aec877ee19f6c335
krushnalchoudhary <krishnalchoudhary9827@gmail.com>
2025-04-14 14:51:18 +0530

    14 Apr

---
+++ Basics/data_types.js
@@ -0,0 +1,6 @@
+"use strict"; //this statement makes the engine to treat your code as newer version of JS
+
+// alert("hello")  
+
+console.log(typeof undefined);
+console.log(typeof null);
---
+++ Basics/first.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail = "krishnal@gmail.com"
+var accountPassword = "12345"
+accountCity = "Burhanpur"
+let accountState;
+//accountId = 1234;
+accountEmail = "kc@gmail.com"
+accountPassword = "67890"
+accountCity = "Pune"
+
+console.table([accountId , accountEmail , accountPassword , accountCity,accountState]);
+
+
