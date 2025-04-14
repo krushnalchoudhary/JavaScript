6534d0c0c96100bce6e8219de681f72c51ce577a
krushnalchoudhary <krishnalchoudhary9827@gmail.com>
2025-04-14 15:07:37 +0530

    Basics

---
+++ Basics/conversion.js
@@ -0,0 +1,39 @@
+let score1 = "33"
+
+let score2 = "123abc";
+
+let score3 = undefined;
+
+let score4 = true;
+
+let score5 = "krishnal"
+
+console.log(typeof score1);
+console.log(typeof score2);
+console.log(typeof score3);
+console.log(typeof score4);
+console.log(typeof score5);
+
+let valuetoNum1 = Number(score1);   //33 => 33
+
+let valuetoNum2 = Number(score2);   //NaN
+
+let valuetoNum3 = Number(score3);    // NaN
+
+let valuetoNum4 = Number(score4);    // true = 1 and false= 0
+
+let valuetoNum5 = Number(score5);   //NaN
+
+console.log(typeof valuetoNum1); 
+console.log(typeof valuetoNum2);
+console.log(typeof valuetoNum3);
+console.log(typeof valuetoNum4);
+console.log(typeof valuetoNum5);
+
+
+console.log(valuetoNum1)
+console.log(valuetoNum2)
+console.log(valuetoNum3)
+console.log(valuetoNum4)
+console.log(valuetoNum5)
+
