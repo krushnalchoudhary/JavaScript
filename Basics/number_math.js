ce4e2abf83beb6cce86c8315d9f5e29a47ad829b
krushnalchoudhary <krishnalchoudhary9827@gmail.com>
2025-04-26 23:15:18 +0530

    Basics

--- Basics/number_math.js
+++ Basics/number_math.js
@@ -1,45 +1,73 @@
 const score = 400;
 console.log(score);
 
-console.log("----------------------------")
+console.log("----------------------------");
 
 const accBalance = new Number(1000);
-console.log(accBalance)
+console.log(accBalance);
 
-console.log("----------------------------")
+console.log("----------------------------");
 
 console.log(accBalance.toString());
 
-console.log("----------------------------")
+console.log("----------------------------");
 
 console.log(accBalance.toString().length);
 
-console.log("----------------------------")
+console.log("----------------------------");
 
-console.log(accBalance.toFixed(2))
+console.log(accBalance.toFixed(2));
 
-console.log("----------------------------")
+console.log("----------------------------");
 
-const num = 1123.8567
-console.log(num.toPrecision(3))
+const num = 1123.8567;
+console.log(num.toPrecision(3));
 
-console.log("----------------------------")
+console.log("----------------------------");
 
 const num2 = 10000000;
-console.log(num2.toLocaleString("en-IN"))
+console.log(num2.toLocaleString("en-IN"));
 
-console.log("----------------------------")
+console.log("----------------------------");
 
-console.log(Number.MAX_VALUE)
+console.log(Number.MAX_VALUE);
 
-console.log("----------------------------")
+console.log("----------------------------");
 
 console.log(Number.MIN_VALUE);
 
-console.log("----------------------------")
+console.log("----------------------------");
 
-console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.MAX_SAFE_INTEGER);
 
-console.log("----------------------------")
+console.log("----------------------------");
 
-console.log(Number.MIN_SAFE_INTEGER)
+console.log(Number.MIN_SAFE_INTEGER);
+
+console.log("----------------------------");
+console.log("Math Library ");
+console.log("----------------------------");
+
+console.log(Math);
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.33));
+
+console.log(Math.ceil(4.1));
+
+console.log(Math.floor(6.99));
+
+console.log(Math.min(4, 3, 8, 7));
+
+console.log(Math.max(4, 3, 8, 9));
+
+console.log(Math.random());
+
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+console.log();
