970211728ebe2138a019c4a6bc2e3763ac63b795
krushnalchoudhary <krishnalchoudhary9827@gmail.com>
2025-04-26 23:00:30 +0530

    Basics

---
+++ Basics/number_math.js
@@ -0,0 +1,45 @@
+const score = 400;
+console.log(score);
+
+console.log("----------------------------")
+
+const accBalance = new Number(1000);
+console.log(accBalance)
+
+console.log("----------------------------")
+
+console.log(accBalance.toString());
+
+console.log("----------------------------")
+
+console.log(accBalance.toString().length);
+
+console.log("----------------------------")
+
+console.log(accBalance.toFixed(2))
+
+console.log("----------------------------")
+
+const num = 1123.8567
+console.log(num.toPrecision(3))
+
+console.log("----------------------------")
+
+const num2 = 10000000;
+console.log(num2.toLocaleString("en-IN"))
+
+console.log("----------------------------")
+
+console.log(Number.MAX_VALUE)
+
+console.log("----------------------------")
+
+console.log(Number.MIN_VALUE);
+
+console.log("----------------------------")
+
+console.log(Number.MAX_SAFE_INTEGER)
+
+console.log("----------------------------")
+
+console.log(Number.MIN_SAFE_INTEGER)
