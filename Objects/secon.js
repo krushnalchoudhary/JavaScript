2a8294e3f0e316c66f8933b0d502a84c25515ad6
krushnalchoudhary <krishnalchoudhary9827@gmail.com>
2025-05-01 14:47:05 +0530

    Basics

---
+++ Objects/secon.js
@@ -0,0 +1,43 @@
+const appUser = new Object(); //single ton object
+
+appUser.id = "123abc";
+appUser.name = "UserName";
+appUser.isLoggedIn = false;
+
+console.log(appUser);
+
+const RegUser = {
+    email : "user@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "userFirstName",
+            lastname: "userLastName"
+        }
+    }
+}
+
+console.log(RegUser.fullname.userfullname);
+
+
+console.log("===================")
+console.log("Object Merging");
+console.log("===================");
+
+const obj1 = {1:"a" , 2: "b"}
+const obj2= {3: "c" , 4: "d"}
+
+const obj3 = {obj1,obj2};
+
+console.log(obj3);
+
+const obj4 = Object.assign({},obj1,obj2)
+console.log(obj4)
+
+
+const obj5 = {...obj1 , ...obj2}
+console.log(obj5);
+
+console.log(Object.keys(appUser));
+console.log(Object.values(appUser));
+console.log(Object.entries(appUser))
+console.log(appUser.hasOwnProperty("name"))
---
+++ Objects/third.js
@@ -0,0 +1,18 @@
+const course = {
+    coursename: "Java Script",
+    price: "999",
+    learningPlateform: "Youtube" 
+}
+
+const {learningPlateform} = course
+
+console.log(learningPlateform)
+
+
+
+
+// const navbar =  ({company}) =>{
+
+// } 
+
+// navbar(company = "companyName")
